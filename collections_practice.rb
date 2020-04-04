3ef0c98947f92329ca857c09cf629003abea1e1b
bruce willis <fashun_vyctum@yahoo.com>
2020-04-04 13:20:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
+def sort_array_asc(array)
+  array = [25, 7, 1]
+  array.sort
+end
 
+def sort_array_desc(array)
+  array = [25, 7, 14]
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array = ["dogs", "cat", "Horses"]
+  arrayeach.length.sort
+end
