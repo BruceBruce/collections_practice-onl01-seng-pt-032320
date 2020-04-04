d2b4e770d17303d685a4d0ef105e78086f5694b8
bruce willis <fashun_vyctum@yahoo.com>
2020-04-04 14:45:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,7 +28,7 @@ end
 def kesha_maker(array)
   kesha = []
   array.each do |word|
-    word_array = word_array.split ""
+    word_array = word.split ""
     word_array[2] = "$"
     kesha = word_array.join
   end
